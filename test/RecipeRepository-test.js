70ffc144d8ff1f024f253bcb92014c0a26143e92
Stephen Tedesco <s_tylert@icloud.com>
2022-04-03 17:12:45 -0600

    Refactor recipeRepository to use one recipe example

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,11 +1,35 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
 import RecipeCard from '../src/classes/RecipeCard';
-const { recipeData } = require('../src/data/recipes');
+//const { recipeData } = require('../src/data/recipes');
 
 describe('Recipe Repository', () => {
   let recipeList;
+  let recipeData;
+
     beforeEach(() => {
+      recipeData = [{"id": 595736,
+      "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+      "ingredients": [
+        {
+          "id": 20081,
+          "quantity": {
+            "amount": 1.5,
+            "unit": "c"
+          }
+        }
+      ],
+      "instructions": [
+        {
+          "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
+          "number": 1
+        }
+      ],
+      "name": "Loaded Chocolate Chip Pudding Cookie Cups",
+      "tags": [
+        "antipasti",
+      ]
+    }]
       recipeList = new RecipeRepository(recipeData)
     });
 
@@ -18,7 +42,7 @@ describe('Recipe Repository', () => {
     })
 
     it('should find recipes by tag', () => {
-      recipeList.getRecipesBySearch('snack')
+      recipeList.getRecipeByTag('antipasti')
       expect(recipeList.recipes).to.includes(recipeData[0])
     })
 
