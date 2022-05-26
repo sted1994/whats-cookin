13c1a3ba92b9fe2e5730dc750185ec68af6a36d7
Stephen Tedesco <s_tylert@icloud.com>
2022-05-26 10:21:41 -0600

    Fix API calls to use deployed api

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,6 @@
-const recipes = fetch('http://localhost:3001/api/v1/recipes').then(response => response.json());
-const ingredients = fetch('	http://localhost:3001/api/v1/ingredients').then(response => response.json());
-const users = fetch('http://localhost:3001/api/v1/users').then(response => response.json());
+const recipes = fetch('http://whats-cookin-api.netlify.app/api/v1/recipes').then(response => response.json());
+const ingredients = fetch('	http://whats-cookin-api.netlify.app/api/v1/ingredients').then(response => response.json());
+const users = fetch('http://whats-cookin-api.netlify.app/api/v1/users').then(response => response.json());
 const data = {
   recipes: recipes,
   users: users,
